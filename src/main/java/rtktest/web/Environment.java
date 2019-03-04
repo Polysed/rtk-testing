3928608a6c32e57d444bbfa004a7e56d06f085a7
Polysh <ladywith@mail.ru>
2019-03-05 01:14:50 +0500

    main page start

---
+++ src/main/java/rtktest/web/Environment.java
@@ -0,0 +1,10 @@
+package rtktest.web;
+
+import org.openqa.selenium.WebDriver;
+
+public class Environment {
+
+    public static WebDriver webDriver;
+
+    public static String applicationUrl = "https://ekt.rt.ru/packages/tariffs";
+}
---
+++ src/main/java/rtktest/web/Utils.java
@@ -0,0 +1,33 @@
+package rtktest.web;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.text.DecimalFormat;
+
+import static rtktest.web.Environment.webDriver;
+
+public class Utils {
+
+//    public static String formatDecimalToString(double value) {
+//        DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+//        return decimalFormat.format(value);
+//    }
+//
+//    @Attachment
+//    public static byte[] makeScreenshoot() {
+//        return ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.BYTES);
+//    }
+
+    public static void scrollIntoElement(WebElement webElement) {
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(webElement).build().perform();
+    }
+
+//    public static void scrollIntoElementWithJavaScript(WebElement webElement) {
+//        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", webElement);
+//    }
+}
